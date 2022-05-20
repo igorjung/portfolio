b487400c8b348fcdee9261f23b4fd7ef1b33dc20
igorjung <igorc.jung@gmail.com>
2022-05-20 18:28:03 -0300

    Adding rewrites

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com','cdn.jsdelivr.net'],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `/:path*`,
+      },
+      {
+        source: "/blog",
+        destination: `${'https://marvelguessr.vercel.app'}/marvelguessr`,
+      },
+      {
+        source: "/blog/:path*",
+        destination: `${'https://marvelguessr.vercel.app'}/marvelguessr/:path*`,
+      },
+    ];
+  },
   reactStrictMode: true,
 }
 
